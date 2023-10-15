34109a69e566e61023f5fbf9a9fd90728e8f31dc
Rupa830904 <rupa.kabyik@gmail.com>
2023-10-15 15:30:07 +0000

    Add product model and add products view

--- products/models.py
+++ products/models.py
@@ -18,8 +18,9 @@ class Package(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    price = models.DecimalField(max_digits=4, decimal_places=2)
-    package_image = CloudinaryField('image', default='placeholder')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    #package_image = CloudinaryField('image', default='placeholder')
+    package_image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
--- products/views.py
+++ products/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from django.db.models.functions import Lower
+from .models import Package, Category
 
 # Create your views here.
+
+def all_products(request):
+    """ A view to show all products, including sorting and search queries """
+
+    products = Package.objects.all()
+    query = None
+    categories = None
+    sort = None
+    direction = None
+
+
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_categories': categories,
+    }
+
+    return render(request, 'products.html', context)
