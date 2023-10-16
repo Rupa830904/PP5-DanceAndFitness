5d0a17ee49586f9b28718c1fa654b6736bc83fb6
Rupa830904 <rupa.kabyik@gmail.com>
2023-10-16 22:15:45 +0000

    Finish main-nav.html , product search and sort

--- products/views.py
+++ products/views.py
@@ -11,15 +11,57 @@ def all_products(request):
 
     products = Package.objects.all()
     query = None
-    categories = None
+    category = None
     sort = None
     direction = None
+
+
+    if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            #if sortkey == 'name':
+             #   sortkey = 'lower_name'
+              #  products = products.annotate(lower_name=Lower('name'))
+            #if sortkey == 'category':
+             #   sortkey = 'category__name'
+            #if 'direction' in request.GET:
+             #   direction = request.GET['direction']
+              #  if direction == 'desc':
+               #     sortkey = f'-{sortkey}'
+            products = products.order_by(sort)
+
+        if 'category' in request.GET:
+                category = request.GET['category']
+                products = products.filter(category__name=category)
+                category = Category.objects.filter(name=category)
+
+        if 'q' in request.GET:
+                query = request.GET['q']
+                if not query:
+                    messages.error(request, "You didn't enter any search!")
+                    return redirect(reverse('products'))
+            
+                queries = Q(name__icontains=query) | Q(description__icontains=query)
+                products = products.filter(queries)
+        
 
 
     context = {
         'products': products,
         'search_term': query,
-        'current_categories': categories,
+        'current_category': category,
     }
 
     return render(request, 'products.html', context)
+
+def product_detail(request, package_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Package, pk=package_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'product_detail.html', context)
