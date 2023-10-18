eb57de17ae273e8be7267d9bed6520b68fe3fba2
Rupa830904 <rupa.kabyik@gmail.com>
2023-10-18 22:15:39 +0000

    Add packages in shopping cart, calculate total , update shopping cart page , add new checkout app

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,17 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Package
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
-    
+    package_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Package, pk=item_id)
+        total += quantity * product.price
+        package_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
+
     context = {
-        'cart_items': bag_items,
+        'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
+        'package_count': package_count,
     }
 
     return context
