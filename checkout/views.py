974553db40321eb3a7c86e55854f0221aeef0c87
Rupa830904 <rupa.kabyik@gmail.com>
2023-10-26 22:26:10 +0000

    Update profile app and form , add stripe element js and env variables

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 
 from .forms import OrderForm
+from cart.contexts import cart_contents
+
+import stripe
 
 
 def checkout(request):
@@ -9,11 +13,17 @@ def checkout(request):
     if not cart:
         messages.error(request, "There's nothing in your cart at the moment")
         return redirect(reverse('products'))
+    
+    current_cart = cart_contents(request)
+    total = current_cart('total')
+    stripe_total = round(total * 100)
 
     order_form = OrderForm()
     template = 'checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51O5au8LpwurLjjCDYB8YIkXPazNfmWvgG0X7uVyisncqHg9dRvnEyZ9r4JZR9sgztIZzOuMeaP1sapyzkGEKGpeL00tAMMTXiC',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
--- danceandfitness/settings.py
+++ danceandfitness/settings.py
@@ -186,3 +186,8 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+STRIPE_CURRENCY = 'usd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
