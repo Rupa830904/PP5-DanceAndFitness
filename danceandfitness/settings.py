f9a84df6609ad7d153ecaca1d9f298e36ee0f429
Rupa830904 <rupa.kabyik@gmail.com>
2023-11-04 23:18:20 +0000

    Add allowed hosts

--- danceandfitness/settings.py
+++ danceandfitness/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = 'django-insecure-5sot&%ov@wz6hh10fz1f=7+rho58e_q5c+mvua-e8e-h(6i6x8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rupa830904-pp5danceandf-q1vb1v6xl1e.ws-eu105.gitpod.io']
+ALLOWED_HOSTS = ['8000-rupa830904-pp5danceandf-q1vb1v6xl1e.ws-eu105.gitpod.io','dancenfitness-708b83044c2d.herokuapp.com']
 
 
 # Application definition
