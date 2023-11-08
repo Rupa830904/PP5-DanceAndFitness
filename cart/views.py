5a5de48df514426c86e1681b48ced6894a00ff5f
Rupa830904 <rupa.kabyik@gmail.com>
2023-11-08 20:37:17 +0000

    Fix update cart

--- cart/views.py
+++ cart/views.py
@@ -17,7 +17,7 @@ def add_to_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
+    redirect_url = request.POST.get('redirect_url', 'view_cart')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
@@ -45,19 +45,47 @@ def remove_from_cart(request, item_id):
 def adjust_cart(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
-    #product = get_object_or_404(Package, pk=item_id)
-    #quantity = int(request.POST.get('quantity'))
-    #cart[item_id] = quantity
+    product = get_object_or_404(Package, pk=item_id)
+    #request.session['cart'] = cart
+    cart = request.session.get('cart', {})
+    print(cart)
+    quantity = cart.get(item_id)
+    print(quantity)
+    cart[item_id] = quantity
     #messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     
-    #request.session['cart'] = cart
     return render(request, 'update_quantity.html')
     #return redirect(reverse('view_cart'))
+    
 def update_cart(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
+    cart = request.session.get('cart', {})
 
-    quantity = int(request.POST.get('quantity'))
-    cart[item_id] = quantity
+    #If the request is POST (user has clicked update)
+    if request.method == 'POST':
+        #Set the quantity from the POST request
+        quantity = int(request.POST.get('quantity'))
+        
+        #If the item is in the list (it should be!), update it
+        if item_id in list(cart.keys()):
+            cart[item_id] = quantity
 
-    request.session['cart'] = cart
-    return redirect(redirect_url)
+        #Send the user back to the cart.
+        request.session['cart'] = cart
+        return redirect('view_cart')
+
+    #Else it's a GET request
+    else:
+
+        #Get the quantity
+        quantity = cart.get(item_id)
+        cart[item_id] = quantity
+
+        #Set the context so we can use it in the template
+        context = {
+            'quantity': quantity
+        }
+
+        #Return the template and the quantity variable
+        request.session['cart'] = cart
+        return render(request, 'update_quantity.html', context)
