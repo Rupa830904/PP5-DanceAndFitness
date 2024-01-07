38ac94e4c2a87b4b633bdb8356c49efb82170abb
Rupa830904 <rupa.kabyik@gmail.com>
2024-01-07 22:07:23 +0000

    Add confirmation email templates and function

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -21,15 +21,13 @@ class StripeWH_Handler:
     def _send_confirmation_email(self, order):
         """Send the user a confirmation email"""
         cust_email = order.email
-        #subject = render_to_string(
-         #   "Dance and Fitness: Order confirmation"
-        #)
-        subject = "Dance and Fitness: Order confirmation"
-        #body = render_to_string(
-        #    {"order": order, "contact_email": settings.DEFAULT_FROM_EMAIL},
-        #)
-        body = "test"
-
+        subject = render_to_string(
+          'confirmation_emails/confirmation_email_subject.txt',
+          {'order': order})
+        body = render_to_string(
+            'confirmation_emails/confirmation_email_body.txt',
+           {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL},
+        )
         send_mail(subject, body, settings.DEFAULT_FROM_EMAIL, [cust_email])
 
     def handle_event(self, event):
@@ -48,13 +46,16 @@ class StripeWH_Handler:
         pid = intent.id
         cart = intent.metadata.cart
         save_info = intent.metadata.save_info
+        
 
         # Get the Charge object
         stripe_charge = stripe.Charge.retrieve(intent.latest_charge)
-        print(stripe_charge)
+        #print(stripe_charge)
 
         billing_details = stripe_charge.billing_details  # updated
         #shipping_details = intent.shipping
+        order_total = round(intent.amount / 100, 2)
+        print(order_total)
 
         # Update profile information if save_info was checked
         profile = None
@@ -63,13 +64,13 @@ class StripeWH_Handler:
         if username != "AnonymousUser":
             profile = UserProfile.objects.get(user__username=username)
             #if save_info:
-            #    profile.default_phone_number = shipping_details.phone
-            #    profile.default_country = shipping_details.address.country
-            #    profile.default_postcode = shipping_details.address.postal_code
-            #    profile.default_town_or_city = shipping_details.address.city
-            #    profile.default_street_address1 = shipping_details.address.line1
-            #    profile.default_street_address2 = shipping_details.address.line2
-            #    profile.default_county = shipping_details.address.state
+            #    profile.default_phone_number = billing_details.phone
+            #    profile.default_country = billing_details.address.country
+            #    profile.default_postcode = billing_details.address.postal_code
+            #    profile.default_town_or_city = billing_details.address.city
+            #    profile.default_street_address1 = billing_details.address.line1
+            #    profile.default_street_address2 = billing_details.address.line2
+            #    profile.default_county = billing_details_details.address.state
             #    profile.save()
 
         order_exists = False
@@ -86,7 +87,7 @@ class StripeWH_Handler:
                     #street_address1__iexact=shipping_details.address.line1,
                     #street_address2__iexact=shipping_details.address.line2,
                     #county__iexact=shipping_details.address.state,
-                    #order_total=order_total,
+                    order_total=order_total,
                     original_cart=cart,
                     stripe_pid=pid,
                 )
@@ -131,14 +132,6 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
-                    ##else:
-                    ##    for size, quantity in item_data["items_by_size"].items():
-                    ##        order_line_item = OrderLineItem(
-                    ##            order=order,
-                    ##            product=product,
-                    ##            quantity=quantity,
-                    ##        )
-                    ##        order_line_item.save()
             except Exception as e:
                 if order:
                     order.delete()
