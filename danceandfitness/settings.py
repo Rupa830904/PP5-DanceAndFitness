309dcd5183f95a00c512933425794a9505b95cf6
Rupa830904 <rupa.kabyik@gmail.com>
2024-01-07 11:29:04 +0000

    change debug

--- danceandfitness/settings.py
+++ danceandfitness/settings.py
@@ -32,8 +32,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5sot&%ov@wz6hh10fz1f=7+rho58e_q5c+mvua-e8e-h(6i6x8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = False
-DEBUG = True
+DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rupa830904-pp5danceandf-q1vb1v6xl1e.ws-eu105.gitpod.io','dancenfitness-708b83044c2d.herokuapp.com','8000-rupa830904-pp5danceandf-q1vb1v6xl1e.ws-eu106.gitpod.io','8000-rupa830904-pp5danceandf-q2lmzgqwfec.ws-eu107.gitpod.io']
 
